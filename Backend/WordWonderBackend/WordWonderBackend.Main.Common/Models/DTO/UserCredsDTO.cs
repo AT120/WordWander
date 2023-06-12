92605a40a9999ae1975e41b6117fb18f6fef2fd5
Alexey <lesha.braun1029@gmail.com>
2023-06-12 19:39:34 +0700

    pull

--- Backend/WordWonderBackend/WordWonderBackend.Main.Common/Models/DTO/UserCredsDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace WordWonderBackend.Main.Common.Models.DTO;
-
-public class UserCredsDTO
-{
-    public required string UserName { get; set; }
-    public required string Password { get; set; }
-}
