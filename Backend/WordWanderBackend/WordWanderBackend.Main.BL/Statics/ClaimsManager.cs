01793762e01ac72970a0ab4120d6854ecd4ac5ea
Artem Trushkin <at.120@ya.ru>
2023-06-12 19:32:06 +0700

    directory rename

--- Backend/WordWanderBackend/WordWanderBackend.Main.BL/Statics/ClaimsManager.cs
+++ Backend/WordWanderBackend/WordWanderBackend.Main.BL/Statics/ClaimsManager.cs
@@ -5,7 +5,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WordWonderBackend.Main.BL.Statics
+namespace WordWanderBackend.Main.BL.Statics
 {
     public static class ClaimsManager
     {
