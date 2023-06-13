dbcc88a1b4ae7f3532ca0b700b3b861088b330ea
Artem Trushkin <at.120@ya.ru>
2023-06-14 00:43:30 +0700

    dynamic color theme

--- Frontend/word-wonder-frontend/src/components/reader/BookViewMin.jsx
+++ Frontend/word-wonder-frontend/src/components/reader/BookViewMin.jsx
@@ -5,6 +5,47 @@ import { useEffect } from "react"
 import { newTextToTranslateThunkCreator, setBookViewActionCreator } from "../../reducers/reader-reducer"
 import { setOnTextChosenCallback } from "../../foliate-js/text-selector"
 
+const hyphenate = false;
+const darkThemeCss = `
+    @namespace epub "http://www.idpf.org/2007/ops";
+    html {
+        color-scheme: light dark;
+    }
+    /* https://github.com/whatwg/html/issues/5426 */
+    @media (prefers-color-scheme: dark) {
+        a:link {
+            color: lightblue;
+        }
+    }
+    p, li, blockquote, dd {
+        line-height: 1.4;
+        text-align: 'justify';
+        -webkit-hyphens: ${hyphenate ? 'auto' : 'manual'};
+        hyphens: ${hyphenate ? 'auto' : 'manual'};
+        -webkit-hyphenate-limit-before: 3;
+        -webkit-hyphenate-limit-after: 2;
+        -webkit-hyphenate-limit-lines: 2;
+        hanging-punctuation: allow-end last;
+        widows: 2;
+    }
+    /* prevent the above from overriding the align attribute */
+    [align="left"] { text-align: left; }
+    [align="right"] { text-align: right; }
+    [align="center"] { text-align: center; }
+    [align="justify"] { text-align: justify; }
+
+    pre {
+        white-space: pre-wrap !important;
+    }
+    aside[epub|type~="endnote"],
+    aside[epub|type~="footnote"],
+    aside[epub|type~="note"],
+    aside[epub|type~="rearnote"] {
+        display: none;
+    }
+`
+// const lightThemeCss
+
 
 function BookViewMin() {
     const bookFile = useSelector(state => state.readerReducer.bookFile)
@@ -17,6 +58,7 @@ function BookViewMin() {
             })
             const view = await getView(bookFile)
             await view.goToTextStart()
+            view.renderer.setStyles(darkThemeCss)
             dispatch(setBookViewActionCreator(view))
         }
 
--- Frontend/word-wonder-frontend/src/index.js
+++ Frontend/word-wonder-frontend/src/index.js
@@ -6,21 +6,30 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import LoginPage from './components/login/loginPage';
 import PrivateWrapper from './PrivateRoute';
 import RegistrationPage from './components/registration/registrationPage';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Reader from './pages/ReaderPage';
-
+import DocumentMeta from 'react-document-meta';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const meta = {
+  meta: {
+    name: {
+      'color-scheme': 'light dark'
+    }
+  }
+}
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route  path='login' element={<LoginPage />} />
-      <Route path='registration' element={<RegistrationPage />} />
-      <Route element={<PrivateWrapper/>}>
-        <Route path='list' element={<BookPage />} />
-      </Route>
-      <Route path="reader" element={<Reader />} />
-    </Routes>
-  </BrowserRouter>,
+  <DocumentMeta {...meta}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='registration' element={<RegistrationPage />} />
+        <Route element={<PrivateWrapper />}>
+          <Route path='list' element={<BookPage />} />
+        </Route>
+        <Route path="reader" element={<Reader />} />
+      </Routes>
+    </BrowserRouter>
+  </DocumentMeta>
 )
