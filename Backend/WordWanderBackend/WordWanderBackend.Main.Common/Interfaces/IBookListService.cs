47922d92c22f966c8574f92a43e6b859bd669a5f
Alexey <lesha.braun1029@gmail.com>
2023-06-15 01:30:04 +0700

    fileLoad

--- Backend/WordWanderBackend/WordWanderBackend.Main.Common/Interfaces/IBookListService.cs
+++ Backend/WordWanderBackend/WordWanderBackend.Main.Common/Interfaces/IBookListService.cs
@@ -14,5 +14,6 @@ namespace WordWanderBackend.Main.Common.Interfaces
         public Task<BooksPaginationDTO> GetUserBooks(int page, string name, Guid userId, BookSortParam? sort);
         public Task PostBookToList(IFormFile file, string title, string description, Guid userId);
         public Task DeleteBookFromList(Guid id, Guid userId);
+        public Task<IFormFile> GetBookById(Guid id, Guid userId);
     }
 }
