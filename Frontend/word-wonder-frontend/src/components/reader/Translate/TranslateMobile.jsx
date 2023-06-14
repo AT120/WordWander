6e7de749cc700a76df0353adfa2d610c4e53b152
Artem Trushkin <at.120@ya.ru>
2023-06-15 00:36:41 +0700

    text scaling

--- Frontend/word-wonder-frontend/src/components/reader/Translate/TranslateMobile.jsx
+++ Frontend/word-wonder-frontend/src/components/reader/Translate/TranslateMobile.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
+function countSpaces(text) {
+    let spaces = 0
+    for (const char of text)
+        if (char == ' ')
+            spaces += 1
+    return spaces
+}
+
+function getTextSize(text) {
+    if (countSpaces(text) <= 1)
+        return 'x-large'
+
+    if (text.length < 50)
+        return 'large'
+
+    if (text.length < 150)
+        return 'medium'
+
+    if (text.length < 200)
+        return 'small'
+}
 
 let prevText = ''
-export default function TranslateMobile({translation}) {
+export default function TranslateMobile({ translation }) {
     const [hidden, hide] = useState(true)
     const hidePopup = () => hide(true)
     const textToTranslate = translation.textToTranslate
@@ -16,22 +37,31 @@ export default function TranslateMobile({translation}) {
     }
 
     return (
-        <div id="translate-popup-mobile" className={`d-flex flex-column ${(hidden) ? 'hidden' : ''}`}>
-            <div className="row flex-grow-1">
-                <div className="text col-6 p-3">
-                    {textToTranslate}
+        <div
+            id="translate-popup-mobile"
+            className={`d-flex flex-column ${(hidden) ? 'hidden' : ''}`}
+            style={{ 'font-size': getTextSize(textToTranslate) }}
+        >
+            <div className="d-flex flex-column">
+
+                    <div className="d-flex">
+                        <div className="col-6 px-3">
+                            {textToTranslate}
+                        </div>
+                        <div className="col-6 px-3">
+                            {(translatedText) ? translatedText : '...'}
+                            {/* TODO: анимация */}
+                        </div>
+                    </div>
+                <div>
+                    <div className="d-flex justify-content-center ">
+                        <button onClick={hidePopup}>
+                            <svg className="icon" viewBox="0 0 25 20" style={{ height: "4rem" }}>
+                                <path d="M 3 15 L 12 12 L 21 15" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <div className="text col-6 p-3">
-                    { (translatedText) ? translatedText : '...'} 
-                    {/* TODO: анимация */}
-                </div>
-            </div>
-            <div className="d-flex justify-content-center">
-                <button onClick={hidePopup}>
-                    <svg className="icon" viewBox="0 0 25 20" style={{ height: "4rem" }}>
-                        <path d="M 3 15 L 12 12 L 21 15" />
-                    </svg>
-                </button>
             </div>
         </div>
     )
