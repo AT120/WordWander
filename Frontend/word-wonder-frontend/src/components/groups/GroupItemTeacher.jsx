9fb4103addd88c00e4a3a52881b138c7f1f5d2cc
Artem Trushkin <at.120@ya.ru>
2023-06-21 13:11:23 +0700

    navigation to member info page

--- Frontend/word-wonder-frontend/src/components/groups/GroupItemTeacher.jsx
+++ Frontend/word-wonder-frontend/src/components/groups/GroupItemTeacher.jsx
@@ -5,15 +5,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { loadStudentsThunkCreator as loadStudentsThunkCreator } from "../../reducers/groups-reducer";
 import UsersDropdown from "./UserDropdown";
+import { useNavigate } from 'react-router-dom';
 
 
 export default function GroupItemTeacher({ name, id }) {
     const students = useSelector(state => state.groupsReducer.students?.[id])
     const [addingIsOpen, setAddingIsOpen] = useState(false);
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     function loadStudents() {
         if (!students)
             dispatch(loadStudentsThunkCreator(id))
+    }
+
+    const goToStudent = () => {
+        navigate("/member-info", { id: id })
     }
 
     return (
@@ -23,26 +30,26 @@ export default function GroupItemTeacher({ name, id }) {
                 <h4 className="d-flex flex-grow-1">{name}</h4>
 
                 <div className="me-2">
-                    {addingIsOpen && <UsersDropdown groupId={id}/>}
+                    {addingIsOpen && <UsersDropdown groupId={id} />}
                 </div>
 
                 <div className="me-5">
 
-                    <AddStudentButton addingIsOpen={addingIsOpen} setAddingIsOpen={setAddingIsOpen}/>
+                    <AddStudentButton addingIsOpen={addingIsOpen} setAddingIsOpen={setAddingIsOpen} />
                     <DeleteGroupButton className='ms-2' groupId={id} />
                 </div>
             </Accordion.Header>
             <Accordion.Body onEntering={loadStudents}>
                 {
                     (!students) ? 'Загрузка...' :
-                    (students.length == 0) ? 'В этой группе никого нет' :
-                        students.map((student) => {
-                            return (
-                                <div className="border-bottom">
-                                    <h6>{student.userName}</h6>
-                                </div>
-                            )
-                        })
+                        (students.length == 0) ? 'В этой группе никого нет' :
+                            students.map((student) => {
+                                return (
+                                    <a className="border-bottom" onClick={goToStudent} style={{cursor: "pointer"}}>
+                                        <h5>{student.userName}</h5>
+                                    </a>
+                                )
+                            })
 
                 }
             </Accordion.Body>
